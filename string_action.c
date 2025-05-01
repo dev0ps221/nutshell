45ad867303a25e4bc65d094a136a0a944f9577b4
dev0ps221 <seybalofficiel@gmail.com>
2025-05-01 12:40:21 +0000

    targ_parser

--- string_action.c
+++ string_action.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 #include<string.h>
 #include"string_action.h"
+
 char* replace_part(const char* str, const char* old, const char* newStr) {
     char* result;
     int i, count = 0;
     int newLen = strlen(newStr);
     int oldLen = strlen(old);
-
     // Count occurrences of old substring
     for (i = 0; str[i] != '\0'; ) {
         if (strstr(&str[i], old) == &str[i]) {
@@ -17,11 +17,9 @@ char* replace_part(const char* str, const char* old, const char* newStr) {
             i++;
         }
     }
-
     // Allocate memory for new string
     result = (char*)malloc(strlen(str) + count * (newLen - oldLen) + 1);
     if (!result) return NULL;
-
     i = 0;
     while (*str) {
         if (strstr(str, old) == str) {
--- targ_parser.c
+++ targ_parser.c
@@ -2,17 +2,13 @@
 #include<stdlib.h>
 #include<string.h>
 #include "string_action.h"
-
-
 //arg format
 struct targ_definition{ char *name; char *shortname; char *value; };
-
 //arg_options
 struct targ_options {
     struct  targ_definition *targs;
     int     targs_size;
 };
-
 // args list
 struct  targ_definition targs[100];
 int     targi = 0;
@@ -22,7 +18,6 @@ void add_cmd_arg(char* name,char* shortname)
     targs[targi] = arg;
     targi++;
 }
-
 struct targ_options get_args(struct  targ_definition *opt_args)
 {
     struct targ_options defi;
@@ -30,46 +25,51 @@ struct targ_options get_args(struct  targ_definition *opt_args)
     defi.targs_size = targi;
     return defi;
 }
-
 struct targ_options parse_args(int argc, char** argv )
 {
-    int i = 0;
+    int i           = 0;
+    int args_met    = 0; 
     struct targ_options options = get_args(0);
     argv++;argc--;
     while (argc > 0) {
         while(i < targi)
         {
+            printf("%d is i and %d is targi",i,targi);
             struct targ_definition targ = options.targs[i];
-
-            if (strcmp(replace_part("--", "", argv[0]), targ.name) == 0 ||
-            strcmp(replace_part("-", "", argv[0]), targ.shortname) == 0) {
-            
+            if  
+            (    
+                    strcmp(replace_part("--", "", argv[0]), targ.name) == 0 
+                ||  
+                    strcmp(replace_part("-", "", argv[0]), targ.shortname) == 0
+            )       
+            {
                 options.targs[i].name = targ.name;
                 options.targs[i].shortname = targ.shortname;
-                options.targs[i].value = (argc > 1) ? ++argv : NULL; // Make sure to check bounds
-                argc--;  // Move to next argument
+                options.targs[i].value = (argc > args_met+1) ? argv[args_met+1] : NULL; // Make sure to check bounds
             }
             argc--; argv++; // Move to next argument
             i++;
+            break;
         }
-    
+        args_met++;
         argc--; argv++; // Move to the next argument if no match found
+        break;
     }
-    return get_args(options.targs);
+    return options;
+    // return get_args(options.targs);
 }
-
 int main(int argc, char** argv)
 {
     int i = 0;
     add_cmd_arg("help","h");
     add_cmd_arg("source","s");
-    
+    printf("here");
     struct targ_options options = parse_args(argc,argv);
+    printf("nere");
     while(i < targi)
     {
         struct targ_definition targ = options.targs[i];
-        printf("%s == %s \n",targ.name,targ.shortname);
+        printf("%s == %s \n",targ.name,targ.value);
         i++;
     }
-
 }
